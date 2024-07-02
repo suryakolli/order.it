11a90911154268d4e2598f2c55a9e6e5e2604146
TharunChamarthi <ch.Tharunkumar@gmail.com>
2024-07-02 18:25:33 -0400

    added angular init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'orderIt' title`, () => {
+  it(`should have the 'temp-project' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('orderIt');
+    expect(app.title).toEqual('temp-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, orderIt');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, temp-project');
   });
 });
