0fd1ab089577130f4beebc5ed651ed874eff3828
TharunChamarthi <36036226+TharunChamarthi@users.noreply.github.com>
2024-07-03 22:26:24 -0400

    created login component (#4)
    * created login component
    
    * Updated Text to lowercase
    
    * Added constants.ts file and used title valible for title
    
    * made change in sytling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+    ],
+  },
+];
